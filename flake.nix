8408955686315785e9d44551f6b120340407919c
Silver <silver@squirl.dev>
2025-08-30 16:33:23 +0100

    use zig 0.15.1

--- flake.nix
+++ flake.nix
@@ -1,10 +1,39 @@
 {
-  outputs = {nixpkgs, ...}: let
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    zig.url = "github:silversquirl/zig-flake";
+    zls.url = "github:zigtools/zls/pull/2457/head";
+
+    zig.inputs.nixpkgs.follows = "nixpkgs";
+    zls.inputs.nixpkgs.follows = "nixpkgs";
+    zls.inputs.zig-overlay.follows = "zig";
+  };
+
+  outputs = {
+    nixpkgs,
+    zig,
+    zls,
+    ...
+  }: let
     forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
   in {
     devShells = forAllSystems (system: pkgs: {
       default = pkgs.mkShellNoCC {
-        packages = with pkgs; [bash zig zls];
+        packages = [
+          pkgs.bash
+          zig.packages.${system}.zig_0_15_1
+          zls.packages.${system}.default
+        ];
+      };
+    });
+
+    packages = forAllSystems (system: pkgs: {
+      default = zig.packages.${system}.zig_0_15_1.makePackage {
+        pname = "zig-flake-template";
+        version = "0.0.0";
+        src = ./.;
+        zigReleaseMode = "fast";
+        # depsHash = "<replace this with the hash Nix provides in its error message>"
       };
     });
   };
